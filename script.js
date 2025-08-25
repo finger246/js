cbc143d8ffeb029efd84c8236633b99cd31e6d4a
finger246 <demenikolaishvili@gmail.com>
2025-08-25 15:41:18 +0400

    'addedSumAnimacias'

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
-
+// VARIABLE
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +26,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +36,175 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//navigacia
+
+btnScrollTo.addEventListener('click', function (e) {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+//tabebi
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+//menus animacia
+
+const hover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const main = e.target;
+    const others = main.closest('.nav').querySelectorAll('.nav__link');
+    const logo = main.closest('.nav').querySelector('img');
+    others.forEach(el => {
+      if (el !== main) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', hover.bind(0.5));
+nav.addEventListener('mouseout', hover.bind(1));
+
+//navigaciis gayinva
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else nav.classList.remove('sticky');
+};
+
+const headerObs = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObs.observe(header);
+
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function () {
+//   if (this.window.scrollY > initialCoords.top) {
+//     nav.classList.add('sticky');
+//   } else nav.classList.remove('sticky');
+// });
+// const obsCallBack = function (entries, observer) {
+//   entries.forEach(entry => console.log(entry));
+// };
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+// const observer = new IntersectionObserver(obsCallBack, obsOptions);
+// observer.observe(section1);
+// const s1cords = section1.getBoundingClientRect();
+// console.log(s1cords);
+// console.log(e.target.getBoundingClientRect());
+// console.log('arsebuli X/Y ', window.pageXOffset, window.pageYOffset);
+// console.log(
+//   'simaghlee da vidsi',
+//   document.documentElement.clientHeight,
+//   document.documentElement.clientWidth
+// );
+// window.scrollTo(
+//   s1cords.left + window.pageXOffset,
+//   s1cords.top + window.pageYOffset
+// );
+// window.scrollTo({
+//   left: s1cords.left + window.pageXOffset,
+//   top: s1cords.top + window.pageYOffset,
+//   behavior: 'smooth',
+// });
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// const alertH1 = function (e) {
+//   alert('addeventlistener: ambatubas');
+//   // h1.removeEventListener('mouseenter', alertH1);
+// };
+// h1.addEventListener('mouseenter', alertH1);
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 4000);
+
+// const messege = document.createElement('div');
+// messege.classList.add('cookie-message');
+// messege.textContent = 'we use cookies for eating';
+// messege.innerHTML =
+//   'we use cookies for eating<button class = "btn btn--close--cookie">Got it </button>';
+// // header.prepend(messege);
+// header.append(messege);
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener('click', function () {
+//     messege.remove();
+//   });
+// messege.style.backgroundColor = '#000';
+// messege.style.width = '120%';
+// messege.style.height =
+//   Number.parseFloat(getComputedStyle(messege).height, 10) + 20 + 'px';
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// const logo = document.querySelector('.nav__logo');
+// logo.alt = 'dingas minimalizmi';
+// logo.setAttribute('company', 'dingiusi');
+// console.log(logo.getAttribute('src'));
+// console.log(logo.src);
+// console.log(logo.alt);
+// console.log(logo.className);
+// console.log(logo.getAttribute('designer'));
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+// console.log(logo.dataset.versionNumber);
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+// const h1 = document.querySelector('h1');
+// console.log(h1.querySelectorAll('.highlight'));
+// //child agheba
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+// //parentis aghebna
+// console.log(h1.parentNode);
+// console.log('......');
+// console.log(h1.parentElement);
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// //siblinebis agheba
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// });
